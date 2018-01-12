9ce1e03648679f00c31ddc09d21bec82f2c23aac
Morgan Martinet <morgan.martinet@ville.montreal.qc.ca>
2018-01-11 22:54:53 -0500

    Update documentation and refactor script a little

--- README.md
+++ README.md
@@ -1,13 +1,48 @@
-This script will stress your Kubernetes node by running all versions of nodejs in order to verify the container image garbage collection.
+# Description
+
+This script will stress your Kubernetes node by running all versions of nodejs in order to verify the garbage collection or the container images.
+
+See https://kubernetes.io/docs/concepts/cluster-administration/kubelet-garbage-collection/
+
+# Steps
 
 It will first pick the first schedulable node, then grab the list of all the tags of the official node repository, from DockerHub.
-Finally, it will execute for each tag a "kubectl run node --version" for the corresponding image.
+Finally, it will execute for each tag a "node --version" for the corresponding image.
+
+# Observe
 
 You can then monitor your disk usage with Prometheus/Grafana and notice the file system grow, then shrink after each cleanup.
 You might encounter failing pods when disk is not sufficient. They won't retry since the restart policy is set to Never.
 
-The pods are run within the docker-stress-test namespace, which is always deleted then created at the beginning of each run.
+# Warning
+
+The pods are run within the *docker-stress-test* namespace, which is always **deleted** then created at the beginning of each run.
 
 You will see the pods in the kubernetes dashboard, but you might not have access to their logs once the garbage collector flushed the completed containers.
 
-See https://kubernetes.io/docs/concepts/cluster-administration/kubelet-garbage-collection/
+# Requirements
+
+- kubectl / docker
+- jq
+- docker-ls
+
+# Start
+
+Ensure that your KUBECONFIG var is properly set and verify the options with the script (namespace, sleep period, tooling...), then run:
+
+```
+bash run-many-different-docker-images.sh
+```
+
+
+# Issues
+
+## Node crash
+
+We experienced node reboots after a few gigs of docker images. 
+
+We had to upgrade the kernel to fix this.
+
+## Garbage collector won't work
+
+We noticed that the data was not garbaged collected because we used either btrfs or xfs with overlay which were not properly monitored by the cadvisor verion of the kubelet.
--- run-many-different-docker-images.sh
+++ run-many-different-docker-images.sh
@@ -2,31 +2,39 @@
 # print commands
 #set -x 
 
-# Install missing tools
-which jq &> /dev/null
-if [ $? -ne 0 ]; then
-    brew install jq
-fi
-which docker-ls &> /dev/null
-if [ $? -ne 0 ]; then
-    brew install docker-ls
-fi
-
 # parameters
 #Tooling options: docker | kubernetes
 #TOOLING=docker
 TOOLING=kubernetes
 KUBERNETES_NAMESPACE=docker-stress-test
-# Find the first node that can be scheduled for pods
-KUBERNETES_NODE=`kubectl get nodes -o json|jq -r '.items[] | select(.spec.unschedulable != true) | .metadata.name' | head -1`
+KUBERNETES_NODE=
 #KUBERNETES_DEBUG="--dry-run"
 DOCKER_REPOSITORY=library/node
+DOCKER_COMMAND=node
+DOCKER_ARGS=--version
 WAIT_TIME=180s
 
+#-------------------------------------------------------------
+checkTool() {
+    which $1 &> /dev/null
+    if [ $? -ne 0 ]; then
+        read -p "brew install $1? [y/n]" input
+        if [ "$input" == "y" ]; then
+            brew install $1
+        else
+            exit 1
+        fi
+    fi
+}
+
+#-------------------------------------------------------------
 initKubernetes() {
     kubectl delete ns $KUBERNETES_NAMESPACE
     sleep 10s
     kubectl create ns $KUBERNETES_NAMESPACE
+
+    # Find the first node that can be scheduled for pods
+    KUBERNETES_NODE=`kubectl get nodes -o json|jq -r '.items[] | select(.spec.unschedulable != true) | .metadata.name' | head -1`
     if [ -z "$KUBERNETES_NODE" ]; then
         echo "Could not find a schedulable kubernetes node!"
         exit 1
@@ -34,6 +42,7 @@ initKubernetes() {
     echo "Scheduling all pods on node $KUBERNETES_NODE"
 }
 
+#-------------------------------------------------------------
 runInKubernetes() {
     POD_NAME=$1
     IMAGE_NAME=$2
@@ -51,34 +60,46 @@ spec:
   - name: test
     image: $IMAGE_NAME
     imagePullPolicy: IfNotPresent
-    command: ["node"]
-    args: ["--version"]
+    command: ["$DOCKER_COMMAND"]
+    args: ["$DOCKER_ARGS"]
 EOF
 }
 
+#-------------------------------------------------------------
+forEachImageTag() {
+    # get a list of images
+    RUN_COUNT=0
+    docker-ls tags $DOCKER_REPOSITORY --json | jq -r '.Tags[]' |  while read tag
+    do
+        RUN_COUNT=$((RUN_COUNT+1))
+        echo "---------------------------------------------------------------------------------------------------------"
+        echo `date` "- Step $RUN_COUNT - test $DOCKER_REPOSITORY/$tag"
+        # run a docker command for each image
+        if [ "$TOOLING" == "kubernetes" ]; then
+            #kubectl run TEST-$RUN_COUNT -n $KUBERNETES_NAMESPACE --image=$DOCKER_REPOSITORY:$tag --restart=Never -- node --version
+            runInKubernetes test-$RUN_COUNT $DOCKER_REPOSITORY:$tag
+        else
+            docker run $DOCKER_REPOSITORY:$tag $DOCKER_COMMAND $DOCKER_ARGS
+            # print disk usage
+            df -H
+        fi
+
+        # wait before running the next command
+        sleep $WAIT_TIME
+    done
+}
+
+#-------------------------------------------------------------
+
+checkTool jq
+checkTool docker-ls
+
 if [ "$TOOLING" == "kubernetes" ]; then
-   initKubernetes
+    initKubernetes
 fi
+    echo "Scheduling all pods on node $KUBERNETES_NODE"
 
-# get a list of images
-RUN_COUNT=0
-docker-ls tags $DOCKER_REPOSITORY --json | jq -r '.Tags[]' |  while read tag
-do
-    RUN_COUNT=$((RUN_COUNT+1))
-    echo "---------------------------------------------------------------------------------------------------------"
-    echo `date` "- Step $RUN_COUNT - test $DOCKER_REPOSITORY/$tag"
-    # run a docker command for each image
-    if [ "$TOOLING" == "kubernetes" ]; then
-        #kubectl run TEST-$RUN_COUNT -n $KUBERNETES_NAMESPACE --image=$DOCKER_REPOSITORY:$tag --restart=Never -- node --version
-        runInKubernetes test-$RUN_COUNT $DOCKER_REPOSITORY:$tag
-    else
-        docker run $DOCKER_REPOSITORY:$tag node --version
-        # print disk usage
-        df -H
-    fi
+forEachImageTag
 
-    # wait before running the next command
-    sleep $WAIT_TIME
-done
 echo "---------------------------------------------------------------------------------------------------------"
 echo `date` "- Done"
